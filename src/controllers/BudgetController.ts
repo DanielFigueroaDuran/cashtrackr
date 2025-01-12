52e0646a88778521ac335d084f2cb6b7a77c06f1
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-12 20:03:44 +0100

    Adding all Routes and Controller Methods

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -7,5 +7,18 @@ export class BudgetController {
 
       static create = async (req: Request, res: Response) => {
             console.log('desde POST /api/budgets');
-      }
+      };
+
+      static getById = async (req: Request, res: Response) => {
+            console.log('desde GetById /api/budgets/id');
+      };
+
+      static updateById = async (req: Request, res: Response) => {
+            console.log('Actualizar con el metodo Put /api/budgets');
+      };
+
+      static deleteById = async (req: Request, res: Response) => {
+            console.log('Eliminar con el metodo Delete /api/budgets');
+      };
+
 }
