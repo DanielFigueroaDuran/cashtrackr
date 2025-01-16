5ae5ced5194b9943de50c4267c4f7ce49eed1970
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-16 21:52:44 +0100

    Adding Controller routes and methods

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -3,6 +3,7 @@ import { BudgetController } from "../controllers/BudgetController";
 import { body, param } from "express-validator";
 import { handleInputErrors } from '../middleware/validation';
 import { validateBudgetExist, validateBudgetId, validateBudgetInput } from '../middleware/budget';
+import { ExpensesController } from "../controllers/ExpenseController";
 
 const router = Router();
 
@@ -28,6 +29,14 @@ router.put('/:budgetId',
 );
 
 router.delete('/:budgetId', BudgetController.deleteById);
+
+/** Routes for expenses */
+
+router.get('/:budgetId/expenses', ExpensesController.getAll);
+router.post('/:budgetId/expenses', ExpensesController.create);
+router.get('/:budgetId/expenses/:expenseId', ExpensesController.getById);
+router.put('/:budgetId/expenses/:expenseId', ExpensesController.updateById);
+router.delete('/:budgetId/expenses/:expenseId', ExpensesController.deleteById);
 
 
 export default router;
