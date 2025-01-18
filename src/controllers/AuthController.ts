c0a533b05d59cea5ca663dc6fae552203396032d
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-18 17:55:11 +0100

    Registering Users

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,17 @@
 import type { Request, Response } from 'express';
+import UserModel from '../models/UserModel';
 
 export class AuthController {
-      static createAccount = (req: Request, res: Response) => {
-            res.json('Creando Cuenta...');
+      static createAccount = async (req: Request, res: Response) => {
+            try {
+                  const user = new UserModel(req.body);
+                  await user.save();
+                  res.json('Cuenta Creada Correctamente');
+
+            } catch (error) {
+                  // console.log(error);
+                  res.status(500).json({ error: 'Hubo un Error' });
+            }
+
       }
 };
