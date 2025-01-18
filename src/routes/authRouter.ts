392361fe36be5582db7f08f6d47cf020b97c60c6
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-18 17:43:33 +0100

    Validation in the User Registry

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/create-account', AuthController.createAccount);
+router.post('/create-account',
+      body('name')
+            .notEmpty().withMessage('El nombre no puede ir vacio'),
+      body('password')
+            .isLength({ min: 8 }).withMessage('El password es muy corto, mínimo 8 caracteres'),
+      body('email')
+            .isEmail().withMessage('E-mail no válido'),
+      handleInputErrors,
+      AuthController.createAccount
+);
 
 export default router
