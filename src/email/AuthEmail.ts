f2493a16efcc4de416ad0871293b89b47e0dba5e
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-19 14:29:03 +0100

    Sending the Account Confirmation Email

--- src/email/AuthEmail.ts
+++ src/email/AuthEmail.ts
@@ -1,3 +1,4 @@
+import { transport } from "../config/nodemailer";
 
 type EmailType = {
       name: string
@@ -7,6 +8,18 @@ type EmailType = {
 
 export class AuthEmail {
       static sendConfirmationEmail = async (user: EmailType) => {
-            console.log(user);
+            //console.log(user);
+            const email = await transport.sendMail({
+                  from: 'CashTrackr <admin@cashtrackr.com>',//si esto no funciona le colocamos @gmail.com
+                  to: user.email,
+                  subject: 'Cashtrackr - Confirma tu cuenta',
+                  html: `
+                        <p>Hola ${user.name}, has creado tu  cuenta en Cashtrackr, ya esta casi lista</p>
+                        <p>Visita el siguiente enlace:</p>
+                        <a href="#">Confirmar Cuenta</a>
+                        <p>e ingresa el código: <b>${user.token}</b></p>
+                  `
+            });
+            console.log('Mensaje enviado ', email.messageId);
       };
 };
