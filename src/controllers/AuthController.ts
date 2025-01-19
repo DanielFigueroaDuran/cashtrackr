6c57d7ac784e8f302cf40326b8e9fab765a8447e
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-19 10:06:30 +0100

    Check that a user exists before registering it

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,17 @@ import UserModel from '../models/UserModel';
 
 export class AuthController {
       static createAccount = async (req: Request, res: Response) => {
+            const { email } = req.body;
+
+            //Prevent Duplicates
+
+            const userExists = await UserModel.findOne({ where: { email } })
+            //res.json(userExists);
+            if (userExists) {
+                  const error = new Error('Un usuario con ese email ya esta registrado');
+                  res.status(409).json({ error: error.message });
+                  return
+            };
             try {
                   const user = new UserModel(req.body);
                   await user.save();
