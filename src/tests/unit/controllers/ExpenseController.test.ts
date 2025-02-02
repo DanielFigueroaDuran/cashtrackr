1c453a41d9b47959068bad03de0ef096294b658f
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-02 20:58:45 +0100

    Testing to Get by ID

--- src/tests/unit/controllers/ExpenseController.test.ts
+++ src/tests/unit/controllers/ExpenseController.test.ts
@@ -1,6 +1,7 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import ExpenseModel from "../../../models/ExpenseModel";
-import { ExpensesController } from "../../../controllers/ExpenseController";
+import { ExpensesController } from '../../../controllers/ExpenseController';
+import { expenses } from "../../mocks/expenses";
 
 jest.mock('../../../models/ExpenseModel', () => ({
       create: jest.fn()
@@ -10,7 +11,7 @@ describe('ExpensesController.create', () => {
 
       it('should create a new expense', async () => {
             const expenseMock = {
-                  save: jest.fn().mockResolvedValue(true)
+                  save: jest.fn()
             };
 
             (ExpenseModel.create as jest.Mock).mockResolvedValue(expenseMock);
@@ -32,5 +33,47 @@ describe('ExpensesController.create', () => {
             expect(expenseMock.save).toHaveBeenCalledTimes(1);
             expect(ExpenseModel.create).toHaveBeenCalledWith(req.body);
 
+      });
+
+      it('should handle expense creation error', async () => {
+            const expenseMock = {
+                  save: jest.fn()
+            };
+
+            (ExpenseModel.create as jest.Mock).mockRejectedValue(new Error);
+
+            const req = createRequest({
+                  method: 'POST',
+                  url: '/api/budgets/:budgetId/expenses',
+                  body: { name: 'Test Expenses', amount: 530 },
+                  budget: { id: 1 }
+            });
+            const res = createResponse();
+
+            await ExpensesController.create(req, res);
+
+            const data = res._getJSONData();
+            expect(res.statusCode).toBe(500);
+            expect(data).toEqual({ error: 'Hubo un error' });
+            expect(expenseMock.save).not.toHaveBeenCalled();
+            expect(ExpenseModel.create).toHaveBeenCalledWith(req.body);
+
+      });
+});
+
+describe('ExpensesController.getById', () => {
+      it('should return expense with ID', async () => {
+            const req = createRequest({
+                  method: 'GET',
+                  url: '/api/budgets/:budgetId/expenses/:expenseId',
+                  expense: expenses[0]
+            });
+            const res = createResponse();
+
+            await ExpensesController.getById(req, res);
+
+            const data = res._getJSONData();
+            expect(res.statusCode).toBe(200);
+            expect(data).toEqual(expenses[0])
       });
 });
