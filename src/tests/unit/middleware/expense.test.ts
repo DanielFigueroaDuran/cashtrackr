4959cfa6a0b879aba29fb86a8869d4c610f5c093
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-05 19:17:15 +0100

    Testing an expense that does not exist

--- src/tests/unit/middleware/expense.test.ts
+++ src/tests/unit/middleware/expense.test.ts
@@ -8,7 +8,25 @@ jest.mock('../../../models/ExpenseModel', () => ({
 }));
 
 describe('Expenses Middleware - validateExpenseExists', () => {
-      it('should hanble a not-existent budget', async () => {
+      beforeEach(() => {
+            (ExpenseModel.findByPk as jest.Mock).mockImplementation((id) => {
+                  const expense = expenses.filter(e => e.id === id)[0] ?? null;
+                  return Promise.resolve(expense);
+            });
+      });
 
+      it('should hanble a not-existent budget', async () => {
+            const req = createRequest({
+                  params: { expenseId: 123 }
+            });
+            const res = createResponse();
+            const next = jest.fn();
+
+            await validateExpenseExist(req, res, next);
+
+            const data = res._getJSONData();
+            expect(res.statusCode).toBe(404);
+            expect(data).toEqual({ error: 'Gasto no encontrado' });
+            expect(next).not.toHaveBeenCalled();
       });
 });
