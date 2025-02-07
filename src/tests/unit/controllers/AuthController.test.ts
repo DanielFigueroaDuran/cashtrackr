ed5cddd8b83a3d4d2ce47770725f361da0d3b78b
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-07 20:32:40 +0100

    Testing to Create a User

--- src/tests/unit/controllers/AuthController.test.ts
+++ src/tests/unit/controllers/AuthController.test.ts
@@ -1,8 +1,12 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import { AuthController } from "../../../controllers/AuthController";
 import UserModel from "../../../models/UserModel";
+import { hashPassword } from "../../../utils/auth";
+import { generateToken } from "../../../utils/token";
 
 jest.mock('../../../models/UserModel');
+jest.mock('../../../utils/auth');
+jest.mock('../../../utils/token');
 
 describe('AuthController.createAccount', () => {
       it('should return a 409 status an error message if the email is already registered', async () => {
@@ -27,5 +31,26 @@ describe('AuthController.createAccount', () => {
             expect(data).toHaveProperty('error', 'Un usuario con ese email ya esta registrado');
             expect(UserModel.findOne).toHaveBeenCalled();
             expect(UserModel.findOne).toHaveBeenCalledTimes(1);
+      });
+
+      it('should register a new user and return a success message', async () => {
+            const req = createRequest({
+                  method: 'POST',
+                  url: '/api/auth/create-account',
+                  body: {
+                        email: "test@test.com",
+                        password: "testpassword"
+                  }
+            });
+
+            const res = createResponse();
+
+            const mockUser = { ...req.body, save: jest.fn() };
+
+            (UserModel.create as jest.Mock).mockResolvedValue(mockUser);
+            (hashPassword as jest.Mock).mockResolvedValue('hashedpassword');
+            (generateToken as jest.Mock).mockReturnValue('123456');
+
+            await AuthController.createAccount(req, res);
       });
 });
