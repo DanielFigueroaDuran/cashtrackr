e1111652c9d4c4bcd2b7f18a22e48dbe49b51b4e
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-08 15:18:04 +0100

    Testing to validation

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  detectOpenHandles: true,
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+    "^.+.tsx?$": ["ts-jest", {}],
+  }
 };
--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -1,15 +1,26 @@
 import request from "supertest";
 import server, { connectDB } from "../../server";
 
-describe('Test', () => {
-      beforeAll(async () => {
-            await connectDB();
-      });
+describe('Authentication - Create Account', () => {
+      // beforeAll(async () => {
+      //       await connectDB();
+      // });
 
-      it('should return a 200 status code from the homepage url', async () => {
-            const response = await request(server).get('/');
+      it('should display validation errors when form is empty', async () => {
+            const response = await request(server)
+                  .post('/api/auth/create-account')
+                  .send({});
 
-            expect(response.statusCode).toBe(200);
-            expect(response.text).toBe('Todo bien...');
+            // console.log(response.body);
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toHaveLength(3);
       });
 });
+
+// {
+//       "name": "Elias",
+//       "password": "12345678",
+//       "email": "figueroadurandanielelias@gmail.com"
+//     }
