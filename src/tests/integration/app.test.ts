2a317085be68f34cfc1cf65ec7eb6ebc7d2de939
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-09 08:17:53 +0100

    Showing messages when the email is invalid

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -20,5 +20,45 @@ describe('Authentication - Create Account', () => {
             expect(response.body.errors).not.toHaveLength(2);
             expect(createAccountMock).not.toHaveBeenCalled();
       });
+
+      it('should return 400 when tje email is invalid', async () => {
+            const response = await request(server)
+                  .post('/api/auth/create-account')
+                  .send({
+                        "name": "Daniel",
+                        "password": "12345678",
+                        "email": "not_valid_email"
+                  });
+
+            const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toHaveLength(1);
+
+            expect(response.status).not.toBe(201);
+            expect(response.body.errors).not.toHaveLength(2);
+            expect(createAccountMock).not.toHaveBeenCalled();
+      });
+
+      // it('should return 400 when tje password is invalid', async () => {
+      //       const response = await request(server)
+      //             .post('/api/auth/create-account')
+      //             .send({
+      //                   "name": "Daniel",
+      //                   "password": "",
+      //                   "email": "test@test.com"
+      //             });
+
+      //       const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+
+      //       expect(response.status).toBe(400);
+      //       expect(response.body).toHaveProperty('errors');
+      //       expect(response.body.errors).toHaveLength(1);
+
+      //       expect(response.status).not.toBe(201);
+      //       expect(response.body.errors).not.toHaveLength(2);
+      //       expect(createAccountMock).not.toHaveBeenCalled();
+      // });
 });
 
