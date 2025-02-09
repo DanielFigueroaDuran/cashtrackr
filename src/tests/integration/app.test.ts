22810246bc344035f00c92e312c41fdc2493f6b1
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-09 10:39:38 +0100

    Testing when everything is correct and a user registers

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -21,7 +21,7 @@ describe('Authentication - Create Account', () => {
             expect(createAccountMock).not.toHaveBeenCalled();
       });
 
-      it('should return 400 when tje email is invalid', async () => {
+      it('should return 400 status code when the email is invalid', async () => {
             const response = await request(server)
                   .post('/api/auth/create-account')
                   .send({
@@ -36,17 +36,19 @@ describe('Authentication - Create Account', () => {
             expect(response.body).toHaveProperty('errors');
             expect(response.body.errors).toHaveLength(1);
 
+            expect(response.body.errors[0].msg).toBe('E-mail no válido');
+
             expect(response.status).not.toBe(201);
             expect(response.body.errors).not.toHaveLength(2);
             expect(createAccountMock).not.toHaveBeenCalled();
       });
 
-      it('should return 400 when tje password is invalid', async () => {
+      it('should return 400 status code when the password is less then 8 characters', async () => {
             const response = await request(server)
                   .post('/api/auth/create-account')
                   .send({
                         "name": "Daniel",
-                        "password": "",
+                        "password": "short",
                         "email": "test@test.com"
                   });
 
@@ -56,9 +58,28 @@ describe('Authentication - Create Account', () => {
             expect(response.body).toHaveProperty('errors');
             expect(response.body.errors).toHaveLength(1);
 
+            expect(response.body.errors[0].msg).toBe('El password es muy corto, mínimo 8 caracteres');
+
             expect(response.status).not.toBe(201);
             expect(response.body.errors).not.toHaveLength(2);
             expect(createAccountMock).not.toHaveBeenCalled();
+      });
+
+      it('should return 201 status code ', async () => {
+
+            const userData = {
+                  "name": "Daniel",
+                  "password": "12345678",
+                  "email": "test@test.com"
+            };
+
+            const response = await request(server)
+                  .post('/api/auth/create-account')
+                  .send(userData);
+
+            expect(response.status).toBe(201);
+            expect(response.status).not.toBe(400);
+            expect(response.body).not.toHaveProperty('errors');
       });
 });
 
